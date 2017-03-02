36d562ede625df89ef17a0a824fdba47a06379a1
manabu.nikaido <nikaiodu.m.aa@m.titech.ac.jp>
2017-03-02 20:54:36 +0900

    Initial Commit

---
+++ lupe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lupe
+//
+//  Created by nikaidomanabu on 2017/03/02.
+//  Copyright © 2017年 manabu.nikaido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
